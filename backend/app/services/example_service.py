14b4dab46717988dda4f0d111f54a264ec54aec6
RafaelMFerreira <rafaelmirandaferreira@outlook.com>
2025-07-21 22:46:46 -0300

    fix: missing import rename

--- backend/app/services/example_service.py
+++ backend/app/services/example_service.py
@@ -1,20 +1,20 @@
 from fastapi import Depends, HTTPException, status
 from app.repository.example_repository import ExampleRepository
-from app.schemas.example import Example, ExampleCreate
+from app.schemas.example import ExampleRead, ExampleCreate
 
 class ExampleService:
     def __init__(self, example_repo: ExampleRepository = Depends()):
         self.example_repo = example_repo
 
-    def get_examples(self) -> list[Example]:
+    def get_examples(self) -> list[ExampleRead]:
         return self.example_repo.get_all_examples()
 
-    def create_new_example(self, example_create: ExampleCreate) -> Example:
+    def create_new_example(self, example_create: ExampleCreate) -> ExampleRead:
         # Here you could add more complex business logic,
         # e.g., checking for duplicates, validating data against other sources, etc.
         return self.example_repo.create_example(example_create)
 
-    def delete_example(self, example_id: int) -> Example:
+    def delete_example(self, example_id: int) -> ExampleRead:
         deleted_example = self.example_repo.delete_example(example_id)
         if not deleted_example:
             raise HTTPException(
@@ -23,7 +23,7 @@ class ExampleService:
             )
         return deleted_example
 
-    def get_example_by_id(self, example_id: int) -> Example:
+    def get_example_by_id(self, example_id: int) -> ExampleRead:
         example = self.example_repo.get_example_by_id(example_id)
         if not example:
             raise HTTPException(
